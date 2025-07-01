8359e52759b6bd58dc5ad2ba229e0cb3d52e75bb
Chathura Ravishan <chathuraravishan73@gmail.com>
2025-07-01 09:47:29 +0530

    Initial FastAPI project with CI/CD and Docker setup
    Set up a FastAPI application with a sample route, health check endpoint, and CORS middleware. Added development and production Dockerfiles, requirements.txt, and a GitHub Actions workflow for CI/CD deploying to Google Cloud Run.

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.api import test_route
+
+import debugpy
+
+debugpy.listen(("0.0.0.0", 5678))
+# debugpy.wait_for_client()
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+app.include_router(test_route.router)
+
+# Define the API endpoints
+@app.get('/')
+def health():
+    return {
+        "message": "OK 🚀"
+    }
